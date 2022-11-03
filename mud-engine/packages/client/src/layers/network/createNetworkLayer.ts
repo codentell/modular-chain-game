79ba801ba083ecde353a9559ca4c390e6105ba3b
codentell <codentell@gmail.com>
2022-11-03 02:20:13 -0700

    add mud engine and connecting with celestia da

---
+++ mud-engine/packages/client/src/layers/network/createNetworkLayer.ts
@@ -0,0 +1,66 @@
+import { createWorld } from "@latticexyz/recs";
+import { setupDevSystems } from "./setup";
+import {
+  createActionSystem,
+  setupMUDNetwork,
+  defineCoordComponent,
+  defineStringComponent,
+} from "@latticexyz/std-client";
+import { defineLoadingStateComponent } from "./components";
+import { SystemTypes } from "contracts/types/SystemTypes";
+import { SystemAbis } from "contracts/types/SystemAbis.mjs";
+import { GameConfig, getNetworkConfig } from "./config";
+import { BigNumber } from "ethers";
+import { Coord } from "@latticexyz/utils";
+
+/**
+ * The Network layer is the lowest layer in the client architecture.
+ * Its purpose is to synchronize the client components with the contract components.
+ */
+export async function createNetworkLayer(config: GameConfig) {
+  console.log("Network config", config);
+
+  // --- WORLD ----------------------------------------------------------------------
+  const world = createWorld();
+
+  // --- COMPONENTS -----------------------------------------------------------------
+  const components = {
+    LoadingState: defineLoadingStateComponent(world),
+    Position: defineCoordComponent(world, { id: "Position", metadata: { contractId: "component.Position" } }),
+    CarriedBy: defineStringComponent(world, { id: "CarriedBy", metadata: { contractId: "component.CarriedBy" } }),
+  };
+
+  // --- SETUP ----------------------------------------------------------------------
+  const { txQueue, systems, txReduced$, network, startSync, encoders } = await setupMUDNetwork<
+    typeof components,
+    SystemTypes
+  >(getNetworkConfig(config), world, components, SystemAbis);
+
+  // --- ACTION SYSTEM --------------------------------------------------------------
+  const actions = createActionSystem(world, txReduced$);
+
+  // --- API ------------------------------------------------------------------------
+  function move(coord: Coord) {
+    systems["system.Move"].executeTyped(BigNumber.from(network.connectedAddress.get()), coord);
+  }
+
+  function pickup(coord: Coord) {
+    systems["system.Catch"].executeTyped(coord);
+  }
+
+  // --- CONTEXT --------------------------------------------------------------------
+  const context = {
+    world,
+    components,
+    txQueue,
+    systems,
+    txReduced$,
+    startSync,
+    network,
+    actions,
+    api: { move, pickup },
+    dev: setupDevSystems(world, encoders, systems),
+  };
+
+  return context;
+}
